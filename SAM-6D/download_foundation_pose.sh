3722bb2f674cd694127f624ed5d300c0e88faa02
savoji <sj.pakdaman.edu@gmail.com>
2025-03-28 14:50:07 +0000

    update script

--- SAM-6D/download_foundation_pose.sh
+++ SAM-6D/download_foundation_pose.sh
@@ -28,6 +28,11 @@ rclone lsd gdrive: --drive-shared-with-me
 
 
 # download dataset to local
-rclone copy "gdrive:train_data" --drive-shared-with-me --progress \
+rclone copy "gdrive:train_data" ./Data/MegaPose-Training-Data/ --drive-shared-with-me --progress \
   --transfers 8 --checkers 16 --buffer-size 64M --multi-thread-streams 4 --fast-list
 
+# move & rename data
+mv ./Data/MegaPose-Training-Data/gso ./Data/MegaPose-Training-Data/FoundationPose-GSO
+mv ./Data/MegaPose-Training-Data/objaverse_path_tracing ./Data/MegaPose-Training-Data/FoundationPose-Objaverse
+
+# symbolic link gso and scn models for foundation pose data too
